b757a4847a960b6355402ca3cd882c842bdcb9d8
chi-qhsun <qhsun_chi@outlook.com>
2024-09-19 17:08:01 -0400

    add static server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//  "public" used to store static data
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

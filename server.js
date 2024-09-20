7378bb118dd5948ccbe4420afc0a71da3e8fd53a
chi-qhsun <qhsun_chi@outlook.com>
2024-09-19 23:03:38 -0400

    Change port back to 80

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

fe34c8f3b27fa01f9ce6dde17908ad6572049ec5
yidanchang <164446952+yidanchangyd@users.noreply.github.com>
2024-11-05 16:54:50 -0500

    change static file path

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//  "public" used to store static data
-app.use(express.static(path.join(__dirname, 'public')));
+//  "public/www" used to store static data
+app.use(express.static(path.join(__dirname, 'public', 'www')));
 
 // HTTP
 const port = process.env.PORT || 80;

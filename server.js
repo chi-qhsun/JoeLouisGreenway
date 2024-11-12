04d75680df96e58265e57fce4e652bd30ec8b67a
Chi Sun <128476547+chi-qhsun@users.noreply.github.com>
2024-11-12 10:48:16 -0500

    Update server.js for static page

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+//const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 //  "public" used to store static data
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/feedback', createProxyMiddleware({ 
-  target: 'http://127.0.0.1:3840', 
-  changeOrigin: true 
-}));
+//app.use('/feedback', createProxyMiddleware({ 
+//  target: 'http://127.0.0.1:3840', 
+//  changeOrigin: true 
+//}));
 
 // HTTP
 const port = process.env.PORT || 80;
